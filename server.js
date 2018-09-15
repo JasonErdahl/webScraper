f29a1e5f8afeb18aae6925f391eafeba017ead31
Jason Erdahl <jasonerdahl@gmail.com>
2018-09-14 22:32:17 -0500

    mongo code for hw 16

--- server.js
+++ server.js
@@ -27,7 +27,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/diggScraper");
+// mongoose.connect("mongodb://localhost/diggScraper");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/diggScraper";
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
