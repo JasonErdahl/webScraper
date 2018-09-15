6387e5c68432cee6097a5c974ec59d16cd2b63d7
Jason Erdahl <jasonerdahl@gmail.com>
2018-09-14 21:35:27 -0500

    update github

--- README (2).md
+++
@@ -1,5 +0,0 @@
-db.getCollection('articles').find({})
-db.getCollection('notes').find({})
-db.articles.remove({})
-db.notes.remove({})
-
--- serverBAK.js
+++ serverBAK.js
@@ -98,7 +98,7 @@ app.get("/scrape", function(req, res) {
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
   // Grab every document in the Articles collection
-  db.Article.find().sort({_id:-1})
+  db.Article.find({})
     .then(function(dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
